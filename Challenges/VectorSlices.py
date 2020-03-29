be0009cf4cda38ecd917586b4b60257df23232d8
DianaBelePublicRepos <dianabeleemea@gmail.com>
2020-03-29 14:17:04 +0100

    Tail reimplementation and vector slices playground added

---
+++ Challenges/VectorSlices.py
@@ -0,0 +1,11 @@
+if len(sys.argv) !=3:
+    print('Usage: VectorSlices.py <file_name> <nlines>')
+    sys.exit(1)
+
+print("*!*!" * 15)
+print(sys.argv[1:])
+print("print(sys.argv[:-1])")
+print(sys.argv[:-1])
+print("print(sys.argv[::-1])")
+print(sys.argv[::-1])
+print(sys.argv[0], sys.argv[1], sys.argv[2],)
