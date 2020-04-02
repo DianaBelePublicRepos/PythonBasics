cee73f48cef1bf3790d8f9cf26ae93bbf8d4ff85
DianaBelePublicRepos <dianabeleemea@gmail.com>
2020-04-02 16:23:40 +0100

    String permutations

---
+++ Challenges/AllPermutationsOfAString.py
@@ -0,0 +1,22 @@
+from itertools import permutations
+
+#With the existing permutations library
+perms = [''.join(p) for p in permutations('goofball')]
+print (perms)
+
+
+#Defining our own function
+def permutations(string, step = 0):
+    if step == len(string):
+        # we've gotten to the end, print the permutation
+        print ("".join(string))
+    for i in range(step, len(string)):
+        # copy the string (store as array)
+        string_copy = [c for c in string]
+         # swap the current index with the step
+        string_copy[step], string_copy[i] = string_copy[i], string_copy[step]
+         # recurse on the portion of the stringthat has not been swapped yet
+        permutations(string_copy, step + 1)
+
+print (permutations ('one'))
+
